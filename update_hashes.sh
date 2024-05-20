87e72ebee8a9f3764faad976c66e2ba29339486c
Diogo L Scarmagnani <diogols@live.com>
2024-05-20 14:25:09 -0300

    with token

--- update_hashes.sh
+++ update_hashes.sh
@@ -8,32 +8,33 @@ get_last_commit() {
   git log -n 1 --pretty=format:%h -- "$1"
 }
 
+# Função para resolver o caminho do arquivo com base no src/href encontrado
+resolve_path() {
+  local path="$1"
+  path=$(echo "$path" | sed -E 's|<\?php echo BASE_URL; \?>|/taramps|g' | sed -E 's|<\?= BASE_URL \?>|/taramps|g')
+  echo "$path"
+}
+
 # Função para processar os arquivos PHP/HTML e atualizar os hashes
 process_file() {
   local file="$1"
 
   # Utiliza o sed para encontrar e substituir as linhas conforme as regras especificadas
   sed -i.bak -E "
-    /<script[^>]*src=[\"'][^\"']*\.(js)[^>]*><\/script>/!b script
-    /data-update-hashes=[\"']false[\"']/b end
-    s|(<script[^>]*src=[\"'][^\"']*)(\.js)([^\"'>]*)([\"'][^>]*><\/script>)|\1\2?v=$(get_last_commit $(resolve_path \1\2))\4|g
-    :script
-    /<link[^>]*href=[\"'][^\"']*\.(css)[^>]*>/!b end
-    /data-update-hashes=[\"']false[\"']/b end
-    s|(<link[^>]*href=[\"'][^\"']*)(\.css)([^\"'>]*)([\"'][^>]*>)|\1\2?v=$(get_last_commit $(resolve_path \1\2))\4|g
-    :end
+    /<script[^>]*src=[\"'][^\"']*\.(js)[^>]*><\/script>/ {
+      /data-update-hashes=[\"']false[\"']/! {
+        s|(<script[^>]*src=[\"'][^\"']*)(\.js)(\?v=[^\"']*)?([\"'][^>]*><\/script>)|\1\2?v=$(get_last_commit $(resolve_path \1\2))\4|g
+      }
+    }
+    /<link[^>]*href=[\"'][^\"']*\.(css)[^>]*>/ {
+      /data-update-hashes=[\"']false[\"']/! {
+        s|(<link[^>]*href=[\"'][^\"']*)(\.css)(\?v=[^\"']*)?([\"'][^>]*>)|\1\2?v=$(get_last_commit $(resolve_path \1\2))\4|g
+      }
+    }
   " "$file"
 
   # Remove o backup gerado pelo sed (sufixo .bak)
   rm "${file}.bak"
-}
-
-# Função para resolver o caminho do arquivo com base no src/href encontrado
-resolve_path() {
-  local path="$1"
-  path=$(echo "$path" | sed 's|<?php echo BASE_URL; ?>|/taramps|g')
-  path=$(echo "$path" | sed 's|<?= BASE_URL ?>|/taramps|g')
-  echo "$path"
 }
 
 # Percorre todos os arquivos PHP e HTML
